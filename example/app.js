8b11cb6480c210443f12182416f9add8002ec672
James Young <jmeyoung@gmail.com>
2016-02-04 08:30:32 -0500

    initial commit

---
+++ example/app.js
@@ -0,0 +1,8 @@
+'use strict';
+require('isomorphic-fetch');
+var path =require('path');
+var health = require('../main');
+
+
+
+let checks = health();
---
+++ example/config/jsonCheckFixture.js
@@ -0,0 +1,25 @@
+'use strict';
+module.exports = {
+	name: 'json check fixture',
+	description: '',
+	checks: [
+		{
+			type: 'json',
+			name: 'CNN Homepage',
+			url: 'http://www.cnn.com/_healthcheck',
+			severity: 2,
+			businessImpact: 'Its a HUGE deal',
+			technicalSummary: 'god knows',
+			panicGuide: 'Don\'t Panic',
+			checkResult: {
+				PASSED: 'Text if check passed',
+				FAILED: 'Text is check failed',
+				PENDING: 'This check has not yet run'
+			},
+			interval: '1s',
+			callback: function(json){
+				return json.propertyToCheck;
+			}
+		}
+	]
+};
