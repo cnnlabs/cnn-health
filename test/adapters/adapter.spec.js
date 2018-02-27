6cc61153b6dab163b9fc2b516d5ca9f76f4b8629
Harry Adams <joey.adams@turner.com>
2018-02-27 01:24:39 -0500

    test: added runCheck() for base adapter

--- test/adapters/adapter.spec.js
+++ test/adapters/adapter.spec.js
@@ -5,12 +5,14 @@ const HealthCheckAdapter = require('../../src/adapters/adapter');
 describe('HealthCheckAdapter', () => {
     const opts = {param: 'value'};
 
-    /**
-     * constructor
-     */
     it('should create adapter with provided options', () => {
         const adapter = new HealthCheckAdapter(opts);
         expect(adapter.options).toBe(opts);
     });
+
+    it('runCheck() should throw error when not overridden', () => {
+        const adapter = new HealthCheckAdapter(opts);
+        expect(adapter.runCheck).toThrow();
+    })
 });
 
